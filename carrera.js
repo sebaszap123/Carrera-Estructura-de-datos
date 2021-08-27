0a9a4a04b325c046e1944c2e4c430772d91ae8b4
sebaszap123 <mfrausto0@ucol.mx>
2021-08-27 17:07:27 -0500

    added class Run and class Career with running, and doCarrer respectively

--- carrera.js
+++ carrera.js
@@ -1,9 +1,38 @@
 // 1/6 para avanzar 3 cuadros
 // 2/6 para avanzar 1 cuadros
 // 3/6 para avanzar 2 cuadros
-let arrMoves = (1, 1, 2, 2, 2, 3);
+const arrMoves = [1, 1, 2, 2, 2, 3];
 class Run {
   constructor(timesrun) {
     this.timesrun = timesrun;
+    this.maxTimes = timesrun.length;
+    this.minTimes = 0;
+  }
+  running() {
+    let times = Math.floor(
+      Math.random() * (this.maxTimes - this.minTimes) + this.minTimes
+    );
+    return this.timesrun[times];
+  }
+}
+
+let newCarrer = new Run(arrMoves);
+console.log(newCarrer.running());
+
+class Career {
+  constructor(run, distance) {
+    this.run = run;
+    this.runners = [0, 0];
+    this.runner1 = "Paola";
+    this.runner2 = "Ximena";
+    this.distance = distance;
+  }
+  doCareer() {
+    let timesRunned = 0;
+    do {
+      this.runners[0] += this.run.running();
+      this.runners[1] += this.run.running();
+      timesRunned++;
+    } while (timesRunned <= this.distance);
   }
 }
