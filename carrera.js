488a1718e96cb6e43c34ebc679d13399c1135e79
sebaszap123 <mfrausto0@ucol.mx>
2021-08-27 16:06:52 -0500

    agregue arrMoves con respectiva probabilidad y clase Run

---
+++ carrera.js
@@ -0,0 +1,9 @@
+// 1/6 para avanzar 3 cuadros
+// 2/6 para avanzar 1 cuadros
+// 3/6 para avanzar 2 cuadros
+let arrMoves = (1, 1, 2, 2, 2, 3);
+class Run {
+  constructor(timesrun) {
+    this.timesrun = timesrun;
+  }
+}
